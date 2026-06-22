47e238901613352da1dca45f5cf65e4f2e1b4d0d
Disha Agarwalla <dishaagarwalla260@gmail.com>
2026-06-22 10:28:21 +0530

    Backend phase completed

--- src/main/java/com/college/resource/controller/BookmarkController.java
+++ src/main/java/com/college/resource/controller/BookmarkController.java
@@ -1,4 +1,4 @@
-package com.college.resource.controller;
+package com.college.resource.controller; 
 
 import com.college.resource.entity.Bookmark;
 import com.college.resource.service.BookmarkService;
